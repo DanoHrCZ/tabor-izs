dcb52ea4f22876ad088fa092474f7f8dd4d23921
DanoHrCZ <daniel.hrecin@gmail.com>
2024-12-16 20:56:12 +0100

    Refactor Firebase configuration to use environment variables and separate config file

--- Firebase.js
+++ Firebase.js
@@ -1,18 +1,8 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD65cha8EuskAmqW9BivLq9p5i6rBNpWxI",
-  authDomain: "tabor-izs.firebaseapp.com",
-  projectId: "tabor-izs",
-  storageBucket: "tabor-izs.firebasestorage.app",
-  messagingSenderId: "19913054232",
-  appId: "1:19913054232:web:beb704dc44be41f1108b6d",
-  measurementId: "G-ZS767CJ7DS"
-};
+import firebaseConfig from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
