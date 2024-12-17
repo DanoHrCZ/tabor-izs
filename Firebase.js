193b5bc3c09341a17a083809d666a2fe909bc512
DanoHrCZ <daniel.hrecin@gmail.com>
2024-12-17 14:53:10 +0100

    Add Firebase configuration directly in Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -2,7 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import firebaseConfig from "./firebaseConfig";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD65cha8EuskAmqW9BivLq9p5i6rBNpWxI",
+    authDomain: "tabor-izs.firebaseapp.com",
+    projectId: "tabor-izs",
+    storageBucket: "tabor-izs.firebasestorage.app",
+    messagingSenderId: "19913054232",
+    appId: "1:19913054232:web:beb704dc44be41f1108b6d",
+    measurementId: "G-ZS767CJ7DS"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
