004cd8aa2262e34943d3fb06e4bb3a3dce6ce2a0
DanoHrCZ <daniel.hrecin@gmail.com>
2024-12-28 12:15:21 +0100

    Update firebaseConfig with production credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
-// firebaseConfig.js
+
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyB5iiRnAN4wW-hGbTqLcq3nOCaMF_22kAc",
+    authDomain: "tabor-izs.firebaseapp.com",
+    projectId: "tabor-izs",
+    storageBucket: "tabor-izs.firebasestorage.app",
+    messagingSenderId: "19913054232",
+    appId: "1:19913054232:web:ef33f2d5f16ef6d6108b6d",
+    measurementId: "G-R6Z4219DJR"
+  };
 
 export default firebaseConfig;
